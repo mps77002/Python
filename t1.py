8c593175257534ede0a6f18d87ac8478aecd71c4
Feng <zhj111@yahoo.com>
2020-02-09 18:39:26 -0600

    test git pysh

---
+++ t1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+test github
+This is a temporary script file.
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+a = [1.0, 2.0, 3.0 ,4.0 ,5.0]
+b = [2.5, 3.2, 4.2, 3.1, 2.3]
+
+plt.plot(a)
+plt.plot(b)
+
